5859f5be6d5edd6565c63235f1bddf415fc5e78d
Prakash <qxprakash@gmail.com>
2025-11-13 16:33:58 +0530

    cleanup all the other tests

--- type-test.ts
+++
@@ -1,8 +0,0 @@
-import Uppy from '@uppy/core'
-import Dashboard from '@uppy/dashboard'
-
-const uppy = new Uppy()
-const d = uppy.getPlugin('Dashboard')
-
-// Force a type error to see what `d` is inferred as
-const forceError: never = d
